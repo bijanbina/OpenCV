daf7dd7485a19bb6e2ce6b86061a2439a59a7fce
bijanbina <bijanbina@gmail.com>
2014-04-20 22:14:11 +0430

    Port to gtk failed

--- Source/example6.c
+++ Source/example6.c
@@ -7,107 +7,89 @@
 #include <gtk/gtk.h>
 
 int g_slider_position = 0;
-int g_slider_position = 0;
 IplImage* image;
 
 
 IplImage* doCanny( IplImage* in, double lowThresh, double highThresh, double aperture )
 {
-    if(in->nChannels != 1)
-    {
-        printf("Not supported\n");
-        return(0); //Canny only handles gray scale images
-    }
-    IplImage* out = cvCreateImage( cvGetSize( in ) , IPL_DEPTH_8U, 1 );
-    cvCanny( in, out, lowThresh, highThresh, aperture );
-    return( out );
+	if(in->nChannels != 1)
+	{
+		printf("Not supported\n");
+		return(0); //Canny only handles gray scale images
+	}
+	IplImage* out = cvCreateImage( cvGetSize( in ) , IPL_DEPTH_8U, 1 );
+	cvCanny( in, out, lowThresh, highThresh, aperture );
+	return( out );
 }
 
 IplImage* doPyrDown( IplImage* in, int filter)
 {
-// Best to make sure input image is divisible by two.
-//
-    assert( in->width%2 == 0 && in->height%2 == 0 );
-    IplImage* out = cvCreateImage(
-                        cvSize( in->width/2, in->height/2 ),
-                        in->depth,
-                        in->nChannels
-                    );
-    cvPyrDown( in, out , CV_GAUSSIAN_5x5);
-    return( out );
+	// Best to make sure input image is divisible by two.
+	//
+	assert( in->width%2 == 0 && in->height%2 == 0 );
+	IplImage* out = cvCreateImage(
+			cvSize( in->width/2, in->height/2 ),
+			in->depth,
+			in->nChannels
+			);
+	cvPyrDown( in, out , CV_GAUSSIAN_5x5);
+	return( out );
 };
 
-void Treshold_change(int pos)
-{
-    double hey = pos;
-    hey = hey / 3.0;
-    IplImage* out = doCanny( image, hey,hey *3, 3 );
-// Do the smoothing
-//
-// Show the smoothed image in the output window
-//
-    cvShowImage( "CV Test", out );
-// Be tidy
-//
-    cvReleaseImage( &out );
-}
 
-void Max_Treshold(int pos)
+void onTrackbarSlide(int pos)
 {
-    double hey = pos;
-    hey = hey / 3.0;
-    IplImage* out = doCanny( image, hey,hey *3, 3 );
-// Do the smoothing
-//
-// Show the smoothed image in the output window
-//
-    cvShowImage( "CV Test", out );
-// Be tidy
-//
-    cvReleaseImage( &out );
+	double hey = pos;
+	hey = hey / 3.0;
+	IplImage* out = doCanny( image, hey,hey *3, 3 );
+	// Do the smoothing
+	//
+	// Show the smoothed image in the output window
+	//
+	cvShowImage( "CV Test", out );
+	// Be tidy
+	//
+	cvReleaseImage( &out );
 }
 
 void example2_4( IplImage *input )
 {
-// Create some windows to show the input
-// and output images in.
-//
-    cvNamedWindow( "output" , CV_WINDOW_AUTOSIZE );
-// Create a window to show our input image
-//
-    cvShowImage( "CV Test-input", input );
-// Create an image to hold the smoothed output
-//
-    cvCreateTrackbar( "Position", "Example4-in", &g_slider_position, 96,
-            Treshold_change
-        );
-    cvCreateTrackbar( "Position", "Example4-in", &g_slider_position, 96,
-            onTrackbarSlide
-        );
-    IplImage* out = doCanny( image, 1,3, 3 );
-// Do the smoothing
-//
-// Show the smoothed image in the output window
-//
-    cvShowImage( "Example4-out", out );
-// Be tidy
-//
-    cvReleaseImage( &out );
-// Wait for the user to hit a key, then clean up the windows
-//
-    cvWaitKey( 0 );
-    cvDestroyWindow( "output" );
-    cvDestroyWindow( "Example4-out" );
+	// Create some windows to show the input
+	// and output images in.
+	//
+	cvNamedWindow( "output" , CV_WINDOW_AUTOSIZE );
+	// Create a window to show our input image
+	//
+	cvShowImage( "CV Test-input", input );
+	// Create an image to hold the smoothed output
+	//
+	cvCreateTrackbar( "Position", "Example4-in", &g_slider_position, 96,
+			onTrackbarSlide
+			);
+	IplImage* out = doCanny( image, 1,3, 3 );
+	// Do the smoothing
+	//
+	// Show the smoothed image in the output window
+	//
+	cvShowImage( "Example4-out", out );
+	// Be tidy
+	//
+	cvReleaseImage( &out );
+	// Wait for the user to hit a key, then clean up the windows
+	//
+	cvWaitKey( 0 );
+	cvDestroyWindow( "output" );
+	cvDestroyWindow( "Example4-out" );
 }
 
 int main( int argc, char** argv )
 {
-    char *file_name = argv[1];
-    if (argv[1] == NULL)
-    {
-    	file_name = "/home/bijan/Pictures/nenit_by_kiko11-d5ka4zo (copy).jpg";
-    }
-    image = cvLoadImage( file_name , CV_LOAD_IMAGE_GRAYSCALE );
-    IplImage *in = cvLoadImage( file_name , CV_LOAD_IMAGE_COLOR );
-    example2_4( in );
+	char *file_name = argv[1];
+	if (argv[1] == NULL)
+	{
+		file_name = "/home/bijan/Pictures/nenit_by_kiko11-d5ka4zo (copy).jpg";
+	}
+	image = cvLoadImage( file_name , CV_LOAD_IMAGE_GRAYSCALE );
+	IplImage *in = cvLoadImage( file_name , CV_LOAD_IMAGE_COLOR );
+	example2_4( in );
 }
--- Source/main.c
+++ Source/main.c
@@ -1,23 +1,66 @@
-#include <opencv/highgui.h>
-int main( int argc, char** argv ) 
+/**
+ * @file GUI.cpp
+ * @brief OpenCV Demo.
+ * @date Created on: 15/03/2010
+ * @author tArKi
+ * @version 1.0
+ */
+
+#include <gtk/gtk.h>
+#include <cv.h>
+#include <highgui.h>
+#include <stdio.h>
+
+gint t = 0;
+IplImage* opencvImage;
+GdkPixbuf* pix;
+
+
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{    
+
+	pix = gdk_pixbuf_new_from_data((guchar*) opencvImage->imageData,
+			GDK_COLORSPACE_RGB, FALSE, opencvImage->depth, opencvImage->width,
+			opencvImage->height, (opencvImage->widthStep), NULL, NULL);
+
+	gdk_cairo_set_source_pixbuf (cr,pix,0,0);
+
+	return FALSE;
+}
+
+int main(int argc, char *argv[])
 {
-	cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
-	CvCapture* capture = cvCreateFileCapture( argv[1] );
-	IplImage* frame;
-	while(1) 
+	GtkWidget *window;
+	GtkWidget *drawing_area;
+	gtk_init (&argc, &argv);
+
+	char *file_name = argv[1];
+	if (argv[1] == NULL)
 	{
-		frame = cvQueryFrame( capture );
-		if( !frame )
-		{
-			break; //exit
-		}
-		cvShowImage( "Example2", frame );
-		char c = cvWaitKey(33);
-		if( c == 27 )
-		{
-			break;
-		}
+		file_name = "/home/bijan/Pictures/nenit_by_kiko11-d5ka4zo (copy).jpg";
 	}
-	cvReleaseCapture( &capture );
-	cvDestroyWindow( "Example2" );
+	//Change this path in order to load any other image.
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	gtk_window_set_title(GTK_WINDOW (window), "RESET DEMO");
+	g_signal_connect (G_OBJECT (window), "destroy",
+			G_CALLBACK (gtk_main_quit), NULL);
+
+	/* Now add a child widget to the aspect frame */
+	drawing_area = gtk_drawing_area_new();
+
+	/* Ask for a 200x200 window, but the AspectFrame will give us a 200x100
+	 * window since we are forcing a 2x1 aspect ratio */
+	//gtk_widget_set_size_request(drawing_area, opencvImage->width, opencvImage->height);
+	//gtk_container_add(GTK_CONTAINER (window), drawing_area);
+	//gtk_widget_show(drawing_area);
+
+
+	//g_signal_connect(G_OBJECT(drawing_area), "draw", 
+	//		G_CALLBACK(on_draw_event), NULL); 
+
+	gtk_widget_show(window);
+	gtk_main();
+	return 0;
 }
